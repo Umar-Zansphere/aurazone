ecffe8bbda6a7e5cd8f171a869fdc854ff78c7a5
Umar Mohamed <umar.mohamed@zansphere.com>
2026-02-27 06:18:23 +0000

    order status changed

---
+++ backend/prisma/migrations/20260227061629_status/migration.sql
@@ -0,0 +1,18 @@
+/*
+  Warnings:
+
+  - The values [PAID,SHIPPED] on the enum `OrderStatus` will be removed. If these variants are still used in the database, this will fail.
+
+*/
+-- AlterEnum
+BEGIN;
+CREATE TYPE "OrderStatus_new" AS ENUM ('PENDING', 'SUCCESS', 'FAILED', 'DELIVERED', 'CANCELLED');
+ALTER TABLE "public"."Order" ALTER COLUMN "status" DROP DEFAULT;
+ALTER TABLE "Order" ALTER COLUMN "status" TYPE "OrderStatus_new" USING ("status"::text::"OrderStatus_new");
+ALTER TABLE "OrderLog" ALTER COLUMN "from_status" TYPE "OrderStatus_new" USING ("from_status"::text::"OrderStatus_new");
+ALTER TABLE "OrderLog" ALTER COLUMN "to_status" TYPE "OrderStatus_new" USING ("to_status"::text::"OrderStatus_new");
+ALTER TYPE "OrderStatus" RENAME TO "OrderStatus_old";
+ALTER TYPE "OrderStatus_new" RENAME TO "OrderStatus";
+DROP TYPE "public"."OrderStatus_old";
+ALTER TABLE "Order" ALTER COLUMN "status" SET DEFAULT 'PENDING';
+COMMIT;
